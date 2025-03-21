cc8280e42c97984d6c2070f57873a84dacc45046
ShivanshuPrajapati212 <152085574+ShivanshuPrajapati212@users.noreply.github.com>
2025-03-21 14:59:19 +0530

    change for deploying

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { configDotenv } from 'dotenv'
+configDotenv()
+
+const BACKEND_URL = process.env.BACKEND_URL
 
 // https://vite.dev/config/
 export default defineConfig({
   server:{
     proxy: {
-      "/api": "http://localhost:3000"
+      "/api": BACKEND_URL
     }
   },
   plugins: [react()],
