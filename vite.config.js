8f12c802fffc5402da40a16ef7db529b42b40de5
ShivanshuPrajapati212 <152085574+ShivanshuPrajapati212@users.noreply.github.com>
2025-03-24 09:56:30 +0530

    update backend url options

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,11 @@ import react from '@vitejs/plugin-react'
 import { configDotenv } from 'dotenv'
 configDotenv()
 
-const BACKEND_URL = process.env.BACKEND_URL
-
 // https://vite.dev/config/
 export default defineConfig({
   server:{
     proxy: {
-      "/api": BACKEND_URL
+      "/api": "https://whimpsyai-backend.vercel.app"
     }
   },
   plugins: [react()],
